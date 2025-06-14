c76392e4bdfdf0432533a0c6c1a7ee61a65ffbf1
Thierno <podiallo2242@gmail.com>
2025-06-14 14:52:00 -0400

    changed the mysql server's password

--- src/main/java/com/pluralsight/dealership/DatabaseConfig.java
+++ src/main/java/com/pluralsight/dealership/DatabaseConfig.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 public class DatabaseConfig {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/DealershipDatabase";
     private static final String DB_USERNAME = "root"; // Change to your MySQL username
-    private static final String DB_PASSWORD = "Salimewol$"; // Change to your MySQL password
+    private static final String DB_PASSWORD = "Mypassword1$"; // Change to your MySQL password
 
     private static BasicDataSource dataSource;
 
